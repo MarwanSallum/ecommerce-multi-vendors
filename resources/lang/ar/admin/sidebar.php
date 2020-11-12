05317946fd60c935b2f7d9de6f6aa1a012c85720
MarwanSallum <maslloum2091@gmail.com>
2020-11-12 14:33:37 +0300

    change admin page direction with language

---
+++ resources/lang/ar/admin/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'settings' => 'الإعدادات',
+    'shipping_methods' => 'وسائل التوصيل',
+    'free_shipping' => 'توصيل مجاني ',
+    'local_shipping' => 'توصيل محلي',
+    'outer_shipping' => 'توصيل خارجي',
+
+];
---
+++ resources/lang/en/admin/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'settings' => 'Settings',
+    'shipping_methods' => 'Shipping Methods',
+    'free_shipping' => 'Free Shipping',
+    'local_shipping' => 'Local Shipping',
+    'outer_shipping' => 'Outer Shipping',
+];
