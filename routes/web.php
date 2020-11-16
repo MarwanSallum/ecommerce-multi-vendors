51d7631461d5962e2b96c3c18128227d81d0e7f5
MarwanSallum <maslloum2091@gmail.com>
2020-11-16 18:04:10 +0300

    enable change language from header

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Setting;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('testLayout', function () {
-    return view('layouts.admin');
+Route::get('test', function () {
+    return Setting::find(21);
 });
